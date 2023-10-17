34ca4c09fcfa91e7af29240ef20d6c0c5651bab1
Maria Eduarda <114308727+Maria-Faria@users.noreply.github.com>
2023-10-16 23:45:57 -0300

    Delete todolist directory

--- todolist/src/main/java/br/com/mariaeduarda/todolist/user/IUserRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.mariaeduarda.todolist.user;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserRepository extends JpaRepository<UserModel, UUID>{
-    UserModel findByUsername(String username);
-}
